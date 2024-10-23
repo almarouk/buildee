7958c485150bb79c3245e2b3b33fe05a90072e1d
Clémentin Boittiaux <boittiauxclementin@gmail.com>
2024-10-23 15:05:49 +0200

    save depth in alpha channel

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ from pathlib import Path
 
 
 # Load blender file
-bpy.ops.wm.open_mainfile(filepath='/home/clementin/Downloads/test2.blend')
+# bpy.ops.wm.open_mainfile(filepath='/home/clementin/Downloads/test2.blend')
 
 # Get the current scene
 scene = bpy.context.scene
@@ -14,29 +14,26 @@ scene = bpy.context.scene
 # Get os temp dir
 temp_dir = Path(tempfile.gettempdir())
 
-# Get temp rgb and depth paths
-rgb_path = temp_dir / 'rgb.exr'
-depth_path = temp_dir / 'depth.exr'
+# Get rendered image path
+render_path = temp_dir / 'render.exr'
+
+# Render the depth in alpha channel
+scene.use_nodes = True
+scene.view_layers["ViewLayer"].use_pass_z = True
+render_node = scene.node_tree.nodes["Render Layers"]
+output_node = scene.node_tree.nodes["Composite"]
+scene.node_tree.links.new(render_node.outputs['Depth'], output_node.inputs['Alpha'])
 
 # Set render settings for rgb output
 scene.render.image_settings.file_format = 'OPEN_EXR'
-scene.render.image_settings.color_mode = 'RGB'
+scene.render.image_settings.color_mode = 'RGBA'
+scene.render.image_settings.color_depth = '32'
 scene.render.image_settings.exr_codec = 'NONE'
-scene.render.filepath = str(rgb_path)
-
-# Set render settings for rgb output
-depth_node = scene.node_tree.nodes["Depth path"]
-depth_node.base_path = str(depth_path.parent)
-depth_node.file_slots[0].path = str(depth_path.stem)
+scene.render.filepath = str(render_path)
 
 for _ in tqdm.tqdm(range(100)):
     # Render the scene
     bpy.ops.render.render(write_still=True)
 
     # Load the image
-    image = bpy.data.images.load(str(rgb_path))
-
-    # Load depth
-    depth = bpy.data.images.load(
-        str(depth_path.parent / f'{depth_path.stem}{scene.frame_current:04d}{depth_path.suffix}')
-    )
+    image = bpy.data.images.load(str(render_path))
